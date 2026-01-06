5f6ebbd41d43e91fc1a132024fb9845d7defd71d
jak57 <jakirhasan718@gmail.com>
2026-01-06 16:58:54 +0600

    inference file preparation done

--- inference.py
+++ inference.py
@@ -1,10 +1,12 @@
 import tensorflow as tf
 from tensorflow.keras.layers import TextVectorization
-import pandas as pd
 import numpy as np
 from config import PATH
 
 def get_vocab():
+  """
+  Returns sorted list of Bengali characters, IPA characters, special tokens and other characters seen in the training set.
+  """
   vb = ['', '[UNK]', '[start]', '[end]', 'া', 'র', '্', 'ে', 'ি', 'ন', 'ক', 'ব', 'স', 'ল', 'ত', 'ম', 'প', 'ু', 'দ', 'ট', 'য়', 'জ', '।', 'ো', 'গ', 'হ', 'য', 'শ', 'ী', 'ই', 'চ', 'ভ', 'আ', 'ও', 'ছ', 'ষ', 'ড', 'ফ', 'অ', 'ধ', 'খ', 'ড়', 'উ', 'ণ', 'এ', 'থ', 'ং', 'ঁ', 'ূ', 'ৃ', 'ঠ', 'ঘ', 'ঞ', 'ঙ', 'ৌ', '‘', 'ৎ', 'ঝ', 'ৈ', '়', 'ঢ', 'ঃ', 'ঈ', '\u200c', 'ৗ', 'a', 'ঐ', 'd', 'w', 'ঋ', 'i', 'e', 't', 's', 'n', 'm', 'b', '“', 'u', 'r', 'œ', 'o', '–', 'ঊ', 'ঢ়', 'Í', 'g', 'p', '\xad', 'h', 'c', 'l', 'ঔ', 'ƒ', '”', 'Ñ', '¡', 'y', 'j', 'f', '→', '—', 'ø', 'è', '¦', '¥', 'x', 'v', 'k']
   vipa = ['', '[UNK]', '[start]', '[end]', 'ɐ', 'ɾ', 'i', 'o', 'e', '̪', 't', 'n', 'k', 'ɔ', 'ʃ', 'b', 'd', 'l', 'u', 'p', 'm', 'ʰ', 'ɟ', '͡', '̯', 'g', 'ʱ', '।', 'c', 'ʲ', 'h', 's', 'ŋ', 'ɛ', 'ɽ', '̃', 'ʷ', '‘', '“', '–', '”', '—', 'w', 'j']
   v = vb + vipa
@@ -15,30 +17,44 @@ def get_vocab():
   return vocab
 
 def get_vectorization():
+  """
+  Performs vectorization.
+  """
   vocab = get_vocab()
   vocab_size = len(vocab)
   sequence_length = 64
-  eng_vectorization = TextVectorization(
+  bn_vectorization = TextVectorization(
       max_tokens=vocab_size, output_mode="int", output_sequence_length=sequence_length,
       vocabulary=vocab
   )
-  spa_vectorization = TextVectorization(
+  ipa_vectorization = TextVectorization(
       max_tokens=vocab_size,
       output_mode="int",
       output_sequence_length=sequence_length + 1,
       vocabulary=vocab
   )
-  return eng_vectorization, spa_vectorization
+  return bn_vectorization, ipa_vectorization
 
-def decode_sequence(input_sentence, eng_vectorization, spa_vectorization, new_model):
+def decode_sequence(input_sentence, bn_vectorization, ipa_vectorization, banglaipa_model):
+    """
+    Generate IPA for subword.
+    
+    Args:
+      - input_sentence (str): Synthetic sentence where every adjacent characters has a space between them.
+      - bn_vectorization: TextVectorization
+      - en_vectorization: TextVectorization
+      - banglaipa_model: Transformer model
+    Returns:
+      - str: String of IPA characters and special tokens where adjacent characters are separated with a space.
+    """
     max_decoded_sentence_length = 64
-    spa_vocab = spa_vectorization.get_vocabulary()
+    spa_vocab = ipa_vectorization.get_vocabulary()
     spa_index_lookup = dict(zip(range(len(spa_vocab)), spa_vocab))
-    tokenized_input_sentence = eng_vectorization([input_sentence])
+    tokenized_input_sentence = bn_vectorization([input_sentence])
     decoded_sentence = '[start]'
     for i in range(max_decoded_sentence_length):
-        tokenized_target_sentence = spa_vectorization([decoded_sentence])[:, :-1]
-        predictions = new_model([tokenized_input_sentence, tokenized_target_sentence])
+        tokenized_target_sentence = ipa_vectorization([decoded_sentence])[:, :-1]
+        predictions = banglaipa_model([tokenized_input_sentence, tokenized_target_sentence])
         sampled_token_index = np.argmax(predictions[0, i, :])
         sampled_token = spa_index_lookup[sampled_token_index]
         decoded_sentence += " " + sampled_token
@@ -46,39 +62,66 @@ def decode_sequence(input_sentence, eng_vectorization, spa_vectorization, new_mo
             break
     return decoded_sentence
 
-def sentence_word(sentence):
+def sentence_to_word(sentence):
+  """
+  Generate word from synthetic sentence by removing spaces between adjacent characters.
+
+  Args:
+    - sentence (str): Synthetic sentence.
+  Returns:
+    - str: subword/word
+  """
   trg=''
   for ch in sentence:
       if ch != " ":
         trg += ch
   return trg
 
-def word_sentence(word):
+def word_to_sentence(word):
+  """
+  Generate synthetic sentence from word by inserting spaces between adjacent characters.
+
+  Args:
+    - word (str): subword/word segement
+  Returns:
+    - str: Synthetic sentence
+  """
   sentence = ""
   for ch in word:
     sentence += (ch + " ")
   return sentence
 
-def get_subword2ipa(word, eng_vectorization, spa_vectorization, new_model):
-    translated = decode_sequence(word_sentence(word), eng_vectorization, spa_vectorization, new_model)
-    trg = sentence_word(translated)
+def get_subword2ipa(word, bn_vectorization, ipa_vectorization, banglaipa_model):
+    translated = decode_sequence(word_to_sentence(word), bn_vectorization, ipa_vectorization, banglaipa_model)
+    trg = sentence_to_word(translated)
     trg = trg[7:]
     trg = trg[:-5]
     return trg
 
 if __name__ == "__main__":
     path = PATH['MODEL_PATH']
-    new_model=tf.saved_model.load(path)
+    banglaipa_model=tf.saved_model.load(path)
     print("BanglaIPA model loaded.")
-    eng_vectorization, spa_vectorization = get_vectorization()
+    bn_vectorization, ipa_vectorization = get_vectorization()
     text = "একটি বাছাই করুন গণিত প্রথম গণিত দ্বিতীয় পত্র"
     ipa = ""
     words = text.split(" ")
     for word in words:
-        trg = get_subword2ipa(word, eng_vectorization, spa_vectorization, new_model)
+        trg = get_subword2ipa(word, bn_vectorization, ipa_vectorization, banglaipa_model)
         print(word, trg)
         ipa += (trg + " ")
-    print(ipa)
+    print(f"IPA={ipa}")
 
 ## python inference.py
+# # Output:
+# BanglaIPA model loaded.
+# একটি ekti
+# বাছাই bɐcʰɐ͡i̯
+# করুন koɾun
+# গণিত gonit̪o
+# প্রথম pɾot̪ʰom
+# গণিত gonit̪o
+# দ্বিতীয় d̪it̪iʲo
+# পত্র pɔt̪ɾo
+# IPA=ekti bɐcʰɐ͡i̯ koɾun gonit̪o pɾot̪ʰom gonit̪o d̪it̪iʲo pɔt̪ɾo
       
